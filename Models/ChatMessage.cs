5b525461fed5bf01939974e4288b5c134888a96d
trumenl <trumenlim@gmail.com>
2021-12-12 14:48:55 +0800

    Added Chat models (ChatMessage & ChatRoom)

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using IdGen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace KampongTalk.Models
 {
     public class ChatMessage
     {
+        public static IdGenerator generator = new IdGenerator(0);
+
+        public string Type { get; set; }
+
+        public string Content { get; set; }
+
+        public string SenderId { get; set; }
+
+        public string MessageId { get; set; }
+
+        public DateTime Timestamp { get; set; }
     }
 }
