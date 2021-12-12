ec7a60062bbc68b951ab9f5fbadacb3caa430f4b
Dylan Kok <me@dylankjy.com>
2021-12-12 14:50:41 +0800

    Cleanup and reformat

--- Models/Relationships.cs
+++ Models/Relationships.cs
@@ -4,10 +4,10 @@
     {
         // Always set UserA as the invoker
         public long UserA { get; set; }
-        
+
         // UserB shall always be the receiving user
         public long UserB { get; set; }
-        
+
         // This property governs the relationship between two users
         // "friends" -> Both users are friends
         // "blocked" -> UserA has blocked UserB
--- Models/UserPreferences.cs
+++ Models/UserPreferences.cs
@@ -4,15 +4,15 @@
     {
         // Contains the UserId that owns this preference
         public long UserId { get; set; }
-        
+
         public bool IsPublic { get; set; } = true;
         public bool UseTts { get; set; } = false;
-        
+
         // Use 2-letter iso code for this field
         public string Language { get; set; } = "en";
-        
+
         public int TextSize { get; set; } = 16;
-        
+
         // This settings govern when an account is considered inactive
         public string DoWhenInactive { get; set; } = "none";
         public int DurationBeforeInactive { get; set; } = 12;
