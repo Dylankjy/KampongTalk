b081a7d2e74a8265e0138fccb3bee995b6b3cb2d
Dylan Kok <me@dylankjy.com>
2021-12-12 14:45:07 +0800

    Community model

---
+++ Models/Community.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using IdGen;
+
+namespace KampongTalk.Models
+{
+    public class Community
+    {
+        public long Cid { get; set; } = new IdGenerator(0).CreateId();
+        public string Name { get; set; }
+        public long CreatorId { get; set; }
+        public string IconImg { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeCreated { get; set; } = DateTime.Now;
+    }
+}
