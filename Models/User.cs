2a17bf7fcaa9d7cd7e22f42f5f90acddd8e921f4
unknown <alexchien09@gmail.com>
2021-12-17 14:48:04 +0800

    Pushing User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,28 @@
 ﻿using IdGen;
+using Microsoft.VisualBasic;
+using Mighty;
 
 namespace KampongTalk.Models
 {
+    [DatabaseTable("Users")]
     public class User
     {
+        [DatabasePrimaryKey]
+        [DatabaseColumn]
         public long Uid { get; set; } = new IdGenerator(0).CreateId();
+        [DatabaseColumn]
         public string PhoneNumber { get; set; }
+        [DatabaseColumn]
         public string Password { get; set; }
+        [DatabaseColumn]
         public string Name { get; set; }
+        [DatabaseColumn]
         public string Bio { get; set; }
-        public string AvatarImg { get; set; }
+        [DatabaseColumn]
+        public string AvatarImg { get; set; } = "default.jpg";
+        [DatabaseColumn]
         public string Interests { get; set; }
+        [DatabaseColumn]
         public string Challenges { get; set; }
     }
 }
