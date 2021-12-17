890f72624241b243d0c8aee66e62dd5cfb730e68
unknown <alexchien09@gmail.com>
2021-12-17 15:53:09 +0800

    Documentation for CRUD db

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,7 +13,7 @@ namespace KampongTalk.Pages
 {
     public class IndexModel : PageModel
     {
-        public User user { get; set; }
+        public dynamic user { get; set; }
 
         private readonly ILogger<IndexModel> _logger;
 
@@ -24,22 +24,33 @@ namespace KampongTalk.Pages
 
         public void OnGet()
         {
-            user = new User()
-            {
-                PhoneNumber = "91223198",
-                Password = "tet",
-                Name = "Nn",
-                Bio = "tt",
-                Interests = "ttt",
-                Challenges = "e"
-            };
+            // Create User here (DONT SPECIFY Uid here)
+            // user = new User()
+            // {
+            //     PhoneNumber = "91223198",
+            //     Password = "tether",
+            //     Name = "My",
+            //     Bio = "Man",
+            //     Interests = "Nothing",
+            //     Challenges = "zero"
+            // };
+            // var db = new MightyOrm(connectionString, "Users");
+            // db.Insert(user);
+
+
+
+            // Retrieving & Updating User (SPECIFY Uid here)
+            // var db = new MightyOrm(connectionString, "Users", "Uid");
+            // db.Insert(user);
+            // var p = db.Single(921304464873226240);
+            // p.Name = "Barry";
+            // db.Update(p);
+
 
             var connectionString = "ProviderName=MySql.Data.MySqlClient;Server=175.156.158.50;Port=50000;Database=kampongtalk;Uid=kamponguser;Pwd=HelloWorld!!1";
-            // var connectionString = "ProviderName=System.Data.SqlClient;data source =175.156.158.50:50000;database =kampongtalk;user id =kamponguser;password = HelloWorld!!1;persist security info = false;";
-            // var connectionString = "ProviderName=System.Data.SqlClient;Data Source=175.156.158.50:50000;Initial Catalog = kampongtalk; User ID = kamponguser; Password = HelloWorld!!1";
             var db = new MightyOrm(connectionString, "Users", "Uid");
-            db.Insert(user);
-            //var p = db.New();
+            // db.Insert(user);
+            user = db.Single(921304464873226240);
         }
     }
 }
