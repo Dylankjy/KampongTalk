b5a396d06b9fb181d0395fffe7b2d287d410ef35
Dylan Kok <me@dylankjy.com>
2021-12-29 00:54:25 +0800

    Added notification options and audio cue
    Changed UserId to Uid

--- Models/UserPreferences.cs
+++ Models/UserPreferences.cs
@@ -2,16 +2,23 @@
 {
     public class UserPreferences
     {
-        // Contains the UserId that owns this preference
-        public long UserId { get; set; }
+        // Contains the Uid that owns this preference
+        public long Uid { get; set; }
 
         public bool IsPublic { get; set; } = true;
         public bool UseTts { get; set; } = false;
+        public bool UseAudioCues { get; set; } = false;
 
         // Use 2-letter iso code for this field
         public string Language { get; set; } = "en";
 
         public int TextSize { get; set; } = 16;
+        
+        // Notification options
+        public bool NotifyTaggedAndReplies { get; set; } = true;
+        public bool NotifyLikes { get; set; } = true;
+        public bool NotifyFriendRequest { get; set; } = true;
+        public bool NotifyChat { get; set; } = true;
 
         // This settings govern when an account is considered inactive
         public string DoWhenInactive { get; set; } = "none";
