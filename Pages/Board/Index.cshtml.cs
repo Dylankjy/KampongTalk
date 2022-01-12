de83a293c3d32f07070c11413795c7002634ae3f
chloryfish <72703739+chloryfish@users.noreply.github.com>
2022-01-12 10:05:40 +0800

    Updates to Board

--- Pages/Board/Index.cshtml.cs
+++ Pages/Board/Index.cshtml.cs
@@ -11,6 +11,7 @@ using Mighty;
 using IdGen;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using MoreLinq;
 
 namespace KampongTalk.Pages.Board
 {
@@ -18,12 +19,46 @@ namespace KampongTalk.Pages.Board
     {
         [BindProperty]
         public Post newPost { get; set; } = new Post();
+
+        [BindProperty]
+        public Likes newLike { get; set; } = new Likes();
+        [BindProperty]
+        public Post newComment { get; set; } = new Post();
         public User CurrentUser { get; set; }
+
+        //public static MightyOrm ktDB { get; set; } =
+        //    new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"]);
 
         public static MightyOrm postDB { get; set; } =
             new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"], "Post");
+
+        public static MightyOrm userDB { get; set; } =
+            new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"], "Users");
+
+        public static MightyOrm likesDB { get; set; } =
+            new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"], "Likes");
+
         [BindProperty]
         public IEnumerable<dynamic> postList { get; set; } = postDB.All();
+
+        [BindProperty]
+        public IEnumerable<dynamic> userList { get; set; } = userDB.All();
+
+        public IEnumerable<dynamic> likesList { get; set; } = likesDB.All();
+
+        //[BindProperty]
+        //public List<dynamic> pList { get; set; } = new List<dynamic>();
+
+        [BindProperty]
+        public IEnumerable<dynamic> pList { get; set; } = Enumerable.Empty<dynamic>() ;
+
+        //    public PagedResults<dynamic> postsList { get; set; } = ktDB.PagedFromSelect(
+        //"Post p INNER JOIN Users u ON p.Author = u.Uid LEFT JOIN Likes li ON p.Pid = li.EntityId",
+        //"p.Timestamp", // order by
+        //"Pid, Content, AttachmentImg, Timestamp, InCommunity, IsComment, TaggedUsers, u.Uid, Name, AvatarImg, COUNT(li.Uid) AS Likes, (SELECT '925664515830317056' IN (SELECT Uid FROM Likes)) AS IsLiked", // columns
+        //"p.IsComment = 0", // OPTIONAL WHERE spec
+        //currentPage: 1, pageSize: 5 // page specs (defaults are 1 and 20)
+        //); // OPTIONAL args for WHERE spec
 
         [BindProperty]
         public IFormFile postImg { get; set; }
@@ -41,10 +76,53 @@ namespace KampongTalk.Pages.Board
             if (CurrentUser == null)
             {
                 return Redirect("/Accounts/Login");
-            }
-            var postsList = postDB.Paged(orderBy: "Timestamp", currentPage: 1, pageSize: 10);
+            };
+            GetPosts();
             return Page();
 
+        }
+
+        public IEnumerable<dynamic> GetPosts()
+        {
+
+            // Query:
+            // Inner join Posts and Users 
+            // Left join Posts and Likes
+            // Fetch count of likes per post
+            // Fetch "isLiked" 
+            // Left join Posts and Communities
+            // "Filter" by requirements (e.g. friends /recc)
+            // Incl pagination
+
+            var query = (from post in postList
+                        join user in userList on post.Author equals user.Uid
+                        join like in likesList on post.Pid equals like.EntityId into likeGrp
+                        from sublike in likeGrp.DefaultIfEmpty()
+                        where post.IsComment == 0
+                        select new PostInfo { Pid = post.Pid, Content = post.Content, AttachmentImg = post.AttachmentImg, 
+                            Timestamp = post.Timestamp, InCommunity = post.InCommunity, IsComment = post.IsComment, TaggedUsers = post.TaggedUsers,
+                            Uid = user.Uid, UserName = user.Name, UserPfp = user.AvatarImg, likeCount = likeGrp.Count()}).DistinctBy(post => post.Pid).AsEnumerable();
+
+            // likeCount = sublike.Uid.Count()
+
+            pList = query;
+
+
+            //if (postType == "friend")
+            //{
+            //    // fetch list of friends from Relationship DB
+            //    // return all posts where author is in list of friend IDs
+            //    return;
+            //}
+            //IEnumerable<dynamic> result = (IEnumerable<dynamic>)ktDB.PagedFromSelect(
+            //    "Post p INNER JOIN Users u ON p.Author = u.Uid, LEFT JOIN Likes li ON p.Pid = li.EntityId", // could have just been a table name!
+            //    "p.Timestamp", // order by
+            //    "Pid, Content, AttachmentImg, Timestamp, InCommunity, IsComment, TaggedUsers, u.Uid, Name, AvatarImg, COUNT(li.Uid) AS Likes, (SELECT '925664515830317056' IN (SELECT Uid FROM Likes)) AS IsLiked", // columns
+            //    "p.IsComment = 0", // OPTIONAL WHERE spec
+            //    currentPage: 1, pageSize: 5 // page specs (defaults are 1 and 20)
+            //    ); // OPTIONAL args for WHERE spec
+
+            return pList;
         }
 
         public IActionResult OnPost()
@@ -70,12 +148,75 @@ namespace KampongTalk.Pages.Board
                 }
                 else
                 {
-                    newPost.AttachmentImg = "";
+                    newPost.AttachmentImg = null;
                 }
                 postDB.Insert(newPost);
                 return Page();
             }
             return Page();
+        }
+
+        public IActionResult OnPostLike()
+        {
+            System.Diagnostics.Debug.WriteLine("onpostlike");
+            CurrentUser = new User().FromJson(HttpContext.Session.GetString("CurrentUser"));
+            var existLike = likesDB.Single(new { EntityId = newLike.EntityId, Uid = CurrentUser.Uid });
+            if (existLike != null)
+            {
+                likesDB.Delete($"EntityId = '{newLike.EntityId}' && Uid = '{CurrentUser.Uid}'");
+            }
+            else
+            {
+                newLike.Uid = CurrentUser.Uid;
+                likesDB.Insert(newLike);
+
+            }
+            return Redirect("/Board");
+        }
+
+        public IActionResult OnPostComment()
+        {
+            CurrentUser = new User().FromJson(HttpContext.Session.GetString("CurrentUser"));
+            newComment.Author = CurrentUser.Uid;
+            newComment.Timestamp = DateTime.Now;
+            newComment.AttachmentImg = null;
+            postDB.Insert(newComment);
+            return Redirect("/Board");
+        }
+
+        //public void likePost(object sender, EventArgs e)
+        //{
+        //    System.Diagnostics.Debug.WriteLine("deceased");
+        //}
+
+        public IEnumerable<dynamic> GetComments(long parentPid)
+        {
+            var query = (from post in postList
+                         join user in userList on post.Author equals user.Uid
+                         join like in likesList on post.Pid equals like.EntityId into likeGrp
+                         from sublike in likeGrp.DefaultIfEmpty()
+                         where post.IsComment == parentPid
+                         select new PostInfo
+                         {
+                             Pid = post.Pid,
+                             Content = post.Content,
+                             AttachmentImg = post.AttachmentImg,
+                             Timestamp = post.Timestamp,
+                             InCommunity = post.InCommunity,
+                             IsComment = post.IsComment,
+                             TaggedUsers = post.TaggedUsers,
+                             Uid = user.Uid,
+                             UserName = user.Name,
+                             UserPfp = user.AvatarImg,
+                             likeCount = likeGrp.Count()
+                         }).DistinctBy(post => post.Pid).AsEnumerable();
+            return query;
+        }
+
+        public void OnPostAddlike()
+        {
+            System.Diagnostics.Debug.WriteLine("Addlike ajax call successful");
+            return;
         }
     }
 }
