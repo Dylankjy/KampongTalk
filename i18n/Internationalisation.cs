0ebda364447b4a949b30fb89088b5d3b33ba1f45
Dylan Kok <me@dylankjy.com>
2022-01-13 00:08:32 +0800

    Update Internationalisation.cs

--- i18n/Internationalisation.cs
+++ i18n/Internationalisation.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.IO;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace KampongTalk.i18n
 {
     public static class Internationalisation
     {
-        public static dynamic LoadLanguage (string langCode)
+        public static dynamic LoadLanguage(string langCode)
         {
-            string json;
-            dynamic languageData;
-                
+            string jsonData;
+            JObject languageData;
+
             try
             {
-                json = File.ReadAllText(@$"{Environment.CurrentDirectory}\i18n\{langCode}.json");
+                jsonData = File.ReadAllText(@$"{Environment.CurrentDirectory}\i18n\{langCode}.json");
             }
             catch (Exception)
             {
-                throw new Exception("Attempted to load a language file that does not exist. Check i18n file's existence and try again.");
+                throw new Exception(
+                    "Attempted to load a language file that does not exist. Check i18n file's existence and try again.");
             }
 
             try
             {
-                languageData = JObject.Parse(json);
+                languageData = JObject.Parse(jsonData);
             }
             catch (Exception)
             {
