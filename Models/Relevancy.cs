1f1be4a51eddd10853499590e3952666b9b76b0a
Dylan Kok <me@dylankjy.com>
2022-01-31 16:36:45 +0800

    "Nope, should be long" -- Chloe

--- Models/Relevancy.cs
+++ Models/Relevancy.cs
@@ -5,7 +5,7 @@ namespace KampongTalk.Models
     public class Relevancy
     {
         // This field can be any Id (Post, User, Event, Community, etc.)
-        public string EntityId { get; set; }
+        public long EntityId { get; set; }
         public string Keyword { get; set; }
         public int Weight { get; set; }
         public DateTime Timestamp { get; set; }
