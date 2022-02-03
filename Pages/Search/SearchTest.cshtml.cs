aca39f87004ede5024571c47272587f154d4c707
Dylan Kok <me@dylankjy.com>
2022-02-04 03:52:45 +0800

    Test files for search

--- Pages/Search/SearchTest.cshtml.cs
+++ Pages/Search/SearchTest.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using KampongTalk.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,7 +9,7 @@ namespace KampongTalk.Pages.Search
     {
         
         [BindProperty] public string SearchQuery { get; set; }
-        public string SearchResult { get; set; }
+        public string SearchResult { get; set; } = String.Empty;
         
         public void OnGet()
         {
@@ -19,9 +20,35 @@ namespace KampongTalk.Pages.Search
         {
             // var result = SearchApi.GetKeyword(SearchQuery).Result;
             
-            SearchApi.PutRelevancy(SearchQuery, 0);
+            // SearchApi.PutRelevancy(SearchQuery, 0);
 
-            SearchResult = "OK";
+            var result = SearchApi.GetSearchResults(SearchQuery).Result;
+
+            foreach (var i in result)
+            {
+                try
+                {
+                    SearchResult += $"{i.EntityId} | {i.Keyword} | {PostApi.GetPostByPid(long.Parse(i.EntityId)).Content}<br>";
+                }
+                catch (Exception e)
+                {
+                    SearchResult += $"{i.EntityId} | {i.Keyword}<br>";
+                }
+                
+                // try
+                // {
+                //     Console.WriteLine("Post");
+                //     long pid = long.Parse(i.EntityId);
+                    // SearchQuery += $"| {PostApi.GetPostByPid(long.Parse(i.EntityId)).Content}<br>";
+                // }
+                // catch
+                // {
+                //     Console.WriteLine("Others");
+                //     SearchQuery += "<br>";
+                // }
+            }
+
+            // SearchResult = "OK";
             
             // SearchResult = $"Nouns: {String.Join(", ", result.nouns)} | Verbs: {String.Join(", ", result.verbs)}";
 
