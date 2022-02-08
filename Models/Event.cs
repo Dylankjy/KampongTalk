8584010bba2d234d8084a8d275514a923e25a6cb
Dylan Kok <me@dylankjy.com>
2022-02-09 03:59:19 +0800

    [EventApi] Add method to get event by id

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using IdGen;
+using Mighty;
 
 namespace KampongTalk.Models
 {
@@ -27,5 +28,17 @@ namespace KampongTalk.Models
         [Required] public string Description { get; set; }
 
         public string Img { get; set; } = "default.jpg";
+    }
+    
+    public static class EventApi 
+    {
+        public static dynamic GetEventById(long id)
+        {
+            var dbEvents =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "Events");
+
+            return dbEvents.Single(new {Eid = id});
+        }
     }
 }
