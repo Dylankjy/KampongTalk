e3ed312a170c94c2a0c9dbdce670af38e884372d
Dylan Kok <me@dylankjy.com>
2022-02-10 03:55:48 +0800

    Visual improvements to profile page

--- Pages/Profile/Index.cshtml.cs
+++ Pages/Profile/Index.cshtml.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
+using IdGen;
 using KampongTalk.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Mighty;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace KampongTalk.Pages.Profile
 {
     public class Index : PageModel
     {
+        private readonly IWebHostEnvironment _environment;
+
+        public Index(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         // Current user prop
         public User CurrentUser { get; set; }
 
@@ -19,6 +31,10 @@ namespace KampongTalk.Pages.Profile
         public dynamic ViewingUser { get; set; }
         public string JoinDate { get; set; }
         public bool IsCurrentUserOwnPage { get; set; }
+
+        // Profile image prop
+        [BindProperty] public IFormFile ProfileImage { get; set; }
+        [BindProperty] public IFormFile BannerImage { get; set; }
 
         // Posts
         public IEnumerable<dynamic> PostsToDisplay { get; set; }
@@ -126,6 +142,89 @@ namespace KampongTalk.Pages.Profile
 
             // Check if birthday is already set, if it hasn't set it
             if (currentUserFromDb.DateOfBirth == null) currentUserFromDb.DateOfBirth = EditBirthday;
+
+            // Profile image
+            if (ProfileImage != null)
+            {
+                var imgExt = ProfileImage.FileName.Split('.').Last();
+                var genImgNum = new IdGenerator(64).CreateId();
+                var genImgName = genImgNum + "." + imgExt;
+                var file = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata", genImgName);
+                var genImgNameOriginal = genImgNum + "-original." + imgExt;
+                var fileOriginal = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata", genImgNameOriginal);
+
+                using (var fileStream = new FileStream(fileOriginal, FileMode.Create))
+                {
+                    ProfileImage.CopyTo(fileStream);
+                }
+
+                using (var image = Image.Load(fileOriginal))
+                {
+                    var ratio = Convert.ToDouble(image.Height) / Convert.ToDouble(image.Width);
+                    var width = 512;
+                    var height = (int) Math.Round(512 * ratio, 0);
+                    image.Mutate(x => x.Resize(width, height));
+
+                    image.Save(file);
+                }
+
+                // if current image is not default.jpg, we will delete on our end
+                if (currentUserFromDb.AvatarImg != "default.jpg")
+                {
+                    string oldAvatarImg = currentUserFromDb.AvatarImg;
+                    var oldAvatarImgOriginal =
+                        oldAvatarImg.Split(".").First() + "-original." + oldAvatarImg.Split(".").Last();
+
+                    var currentImgPath = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata", oldAvatarImg);
+                    var currentImgPathOriginal = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata",
+                        oldAvatarImgOriginal);
+                    System.IO.File.Delete(currentImgPath);
+                    System.IO.File.Delete(currentImgPathOriginal);
+                }
+
+                currentUserFromDb.AvatarImg = genImgName;
+            }
+
+            if (BannerImage != null)
+            {
+                var imgExt = BannerImage.FileName.Split('.').Last();
+                var genImgNum = new IdGenerator(65).CreateId();
+                var genImgName = genImgNum + "-banner." + imgExt;
+                var file = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata", genImgName);
+                var genImgNameOriginal = genImgNum + "-banner-original." + imgExt;
+                var fileOriginal = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata", genImgNameOriginal);
+
+                using (var fileStream = new FileStream(fileOriginal, FileMode.Create))
+                {
+                    BannerImage.CopyTo(fileStream);
+                }
+
+                using (var image = Image.Load(fileOriginal))
+                {
+                    var ratio = Convert.ToDouble(image.Height) / Convert.ToDouble(image.Width);
+                    var width = 512;
+                    var height = (int) Math.Round(512 * ratio, 0);
+                    image.Mutate(x => x.Resize(width, height));
+
+                    image.Save(file);
+                }
+
+                // if current image is not default.jpg, we will delete on our end
+                if (currentUserFromDb.BannerImg != "default-banner.jpg")
+                {
+                    string oldBannerImg = currentUserFromDb.BannerImg;
+                    var oldBannerImgOriginal =
+                        oldBannerImg.Split(".").First() + "-original." + oldBannerImg.Split(".").Last();
+
+                    var currentImgPath = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata", oldBannerImg);
+                    var currentImgPathOriginal = Path.Combine(_environment.ContentRootPath, "wwwroot/userdata",
+                        oldBannerImgOriginal);
+                    System.IO.File.Delete(currentImgPath);
+                    System.IO.File.Delete(currentImgPathOriginal);
+                }
+
+                currentUserFromDb.BannerImg = genImgName;
+            }
 
             // Add audit log
             var editLog = new ActionLog
