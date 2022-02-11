763a9613d121e63b54011c58ad56e47387a512c6
Dylan Kok <me@dylankjy.com>
2022-02-11 15:59:01 +0800

    Update chatmessage table
    Co-Authored-By: trumenl <76671669+trumenl@users.noreply.github.com>

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -6,13 +6,12 @@ namespace KampongTalk.Models
     public class ChatMessage
     {
         public long MessageId { get; set; } = new IdGenerator(0).CreateId();
-
-        //message type
-        public string Type { get; set; }
+        
+        public long RecipientId { get; set; }
 
         public string Content { get; set; }
 
-        public string SenderId { get; set; }
+        public long SenderId { get; set; }
 
         public DateTime Timestamp { get; set; }
     }
