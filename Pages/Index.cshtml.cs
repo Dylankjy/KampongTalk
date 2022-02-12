42727007611b942068f29da2de1bd7dfee538f42
Dylan Kok <me@dylankjy.com>
2022-02-13 04:07:15 +0800

    Added landing page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,23 @@
-﻿using KampongTalk.Models;
+﻿using System;
+using System.Linq;
+using IdGen;
+using KampongTalk.i18n;
+using KampongTalk.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using Mighty;
 
 namespace KampongTalk.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        // private readonly ILogger<IndexModel> _logger;
+        //
+        // public IndexModel(ILogger<IndexModel> logger)
+        // {
+        //     _logger = logger;
+        // }
 
         // Use dynamic here, not User. Because MightyORM does not return a User object
         public User CurrentUser { get; set; }
@@ -25,47 +28,126 @@ namespace KampongTalk.Pages
 
         public dynamic LangData { get; set; }
 
+        public bool IsLoggedIn { get; set; }
+
+
+        // Prop declarations
+        [BindProperty] public string LoginPhoneNumber { get; set; }
+        [BindProperty] public string LoginPassword { get; set; }
+        public string FieldClass { get; set; }
+        public bool ShowErrorMessage { get; set; }
+
         public IActionResult OnGet()
         {
-            // Create User here (DONT SPECIFY Uid in MightyOrm Constructor here)
-            // user = new User()
-            // {
-            //     PhoneNumber = "91223198",
-            //     Password = "tether",
-            //     Name = "My",
-            //     Bio = "Man",
-            //     Interests = "Nothing",
-            //     Challenges = "zero"
-            // };
-            // var db = new MightyOrm(connectionString, "Users");
-            // db.Insert(user);
-
-
-            // Retrieving & Updating User (SPECIFY Uid here)
-            // var db = new MightyOrm(connectionString, "Users", "Uid");
-            // db.Insert(user);
-            // var p = db.Single(921304464873226240);
-            // p.Name = "Barry";
-            // db.Update(p);
-
             var db = new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
                 "Users");
 
             CurrentUser = new User().FromJson(HttpContext.Session.GetString("CurrentUser"));
+
+            // Set user language
             if (CurrentUser == null)
             {
-                //CurrentUser = new User();
-                return Redirect("/Accounts/Login");
+                LangData = Internationalisation.LoadLanguage(HttpContext.Request.GetTypedHeaders().AcceptLanguage
+                    .First().ToString().Split("-").First());
             }
-            LangData = UserPrefApi.GetLangByUid(CurrentUser);
-            var preferencesDB = new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"], "UserPreferences");
+            else
+            {
+                LangData = UserPrefApi.GetLangByUid(CurrentUser);
+            }
+
+            if (CurrentUser == null)
+            {
+                IsLoggedIn = false;
+                return Page();
+            }
+
+            IsLoggedIn = true;
+
+            var preferencesDB =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "UserPreferences");
 
             var uid = CurrentUser.Uid;
-            dynamic loggedInUserPrefs = preferencesDB.Single($"Uid = {uid}");
+            var loggedInUserPrefs = preferencesDB.Single($"Uid = {uid}");
             useAudioCues = loggedInUserPrefs.UseAudioCues;
 
             textSize = "larger";
+
             return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            // Database declarations
+            var dbActionLogs =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "ActionLogs");
+            var dbUsers =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "Users");
+
+            var dbPrefs =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "UserPreferences");
+
+            // Block OnPost if user is verified and already authenticated
+            var currentUser = new User().FromJson(HttpContext.Session.GetString("CurrentUser"));
+            if (currentUser is {IsVerified: true}) return RedirectToPage("Index");
+
+            if (currentUser == null)
+            {
+                LangData = Internationalisation.LoadLanguage(HttpContext.Request.GetTypedHeaders().AcceptLanguage
+                    .First().ToString().Split("-").First());
+            }
+            else
+            {
+                LangData = UserPrefApi.GetLangByUid(CurrentUser);
+            }
+            
+
+            // Get user by PhoneNumber
+            var selectedUserFromDb = dbUsers.Single(new
+            {
+                PhoneNumber = LoginPhoneNumber
+            });
+
+            // If user doesn't exist, initialise new user to prevent timing attacks
+            if (selectedUserFromDb == null)
+            {
+                selectedUserFromDb = new User();
+                selectedUserFromDb.SetPassword(new IdGenerator(0).CreateId().ToString());
+            }
+
+            // Convert dynamic to user object
+            User selectedUser = new User().ToUser(selectedUserFromDb);
+
+            // Compare password
+            if (!selectedUser.ComparePassword(LoginPassword))
+            {
+                ShowErrorMessage = true;
+                FieldClass = "is-danger";
+                return Page();
+            }
+
+            // If all is well, set the user into session
+            // Set the session
+            HttpContext.Session.SetString("CurrentUser", selectedUser.ToJson());
+
+            // Redirect to verification page if unverified
+            if (!selectedUser.IsVerified) return RedirectToPage("Verify");
+
+            // Generate new OTP code and insert into DB
+            var otpRecord = new ActionLog
+            {
+                Uid = selectedUser.Uid,
+                ActionExecuted = "account_login_success",
+                Metadata = null,
+                Info = "Successful login attempt performed on your account."
+            };
+            dbActionLogs.Insert(otpRecord);
+
+            // Else, go to index
+            return Redirect("/");
         }
     }
 }
