8eefcff0f9af187082855c7c596b42d7b205b6a1
Dylan Kok <me@dylankjy.com>
2022-02-13 00:45:40 +0800

    [RelApi] Add method to retrieve privacy settings

--- Models/Relationships.cs
+++ Models/Relationships.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using IdGen;
 using Mighty;
 
@@ -19,5 +20,62 @@ namespace KampongTalk.Models
         // "blocked" -> UserA has blocked UserB
         // "pending" -> UserA has sent UserB a friend request, but UserB has yet to accept it
         [DatabaseColumn] [Required] public string Status { get; set; } = "pending";
+    }
+
+    public static class RelApi
+    {
+        public static bool IsAccessible(long currentUid, long viewingUid)
+        {
+            var dbPrefs =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "UserPreferences");
+            var dbRelation =
+                new MightyOrm(ConfigurationManager.AppSetting["ConnectionStrings:KampongTalkDbConnection"],
+                    "Relationships");
+
+            var viewingUserPref = dbPrefs.Single(new {Uid = viewingUid});
+
+            if (viewingUserPref.IsPublic == true || currentUid == viewingUid)
+            {
+                return true;
+            }
+            
+            // Get friend list
+            var friendListRaw =
+                dbRelation.Query(
+                    $"Select * from Relationships where (UserA = '{viewingUid}' or UserB = '{viewingUid}') AND Status = 'friends'");
+
+            bool isFriends = false;
+            List<dynamic> friendList = new List<dynamic>();
+
+            if (friendListRaw != null)
+            {
+                foreach (var relationRow in friendListRaw)
+                {
+                    if (relationRow.UserA != viewingUid)
+                    {
+                        friendList.Add(UserApi.GetUserById(relationRow.UserA));
+                    }
+                    else
+                        friendList.Add(UserApi.GetUserById(relationRow.UserB));
+                }
+
+                // Check whether the current user is a friend
+                foreach (var row in friendList)
+                {
+                    if (currentUid == row.Uid)
+                    {
+                        isFriends = true;
+                    }
+                }
+            }
+            
+            if (!isFriends)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
