d9c05bf8fd05058877ea39b9331a5218e3851258
Dylan Kok <me@dylankjy.com>
2022-02-12 16:07:52 +0800

    Hi Chloe, logout now works

---
+++ Pages/Accounts/Logout.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KampongTalk.Pages.Accounts
+{
+    public class Logout : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Redirect("/");
+        }
+        
+        public IActionResult OnPost()
+        {
+            HttpContext.Session.Clear();
+            foreach (var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+
+            return Redirect("/");
+        }
+    }
+}
